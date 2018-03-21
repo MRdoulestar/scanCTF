7261d7a115072f993972b115a69ea796128d285d
star double <yunsle@-yyqdeMacBook-Air.local>
2018-03-21 18:23:59 +0800

    update

--- scan.py
+++ scan.py
@@ -1,38 +1,50 @@
 #coding=utf-8
+
+import threading
 import requests
 import os
 import sys
+import Queue
+
+q = Queue.Queue()
+thread_num = 6
+
 def getFuzzUrl():
 	try:
 		f = open('./dictionary/default.txt','r')
 		lines = f.readlines()
 		f.close()
-		data=[]
 		for line in lines:
-			data.append(line.strip('\n'))
-		return data
-	except:
-		print("ERROR:::Can't open the dictionary.")
-
-def run(url):
+			q.put(line.rstrip('\n'))
+	except Exception as e:
+		print(e)
+def request(url):
 	r = requests.get(url)
 	return r.status_code
 
+def scan():
+	# Start scanning
+	while not q.empty():
+		extUrl = q.get()
+		code = request(baseUrl+extUrl)
+		if code!=404:
+			print(str(code)+"--------------------"+baseUrl+extUrl)
+	
+
 if __name__ == '__main__':
 	try:
-		baseUrl = sys.argv[1]
-	except:
+		baseUrl = sys.argv[1].rstrip('/')
+	except Exception as e:
+		print(e)
 		print("ERROR:::Please input the baseurl.")
 		sys.exit(1)
 	try:
-		code = run(baseUrl)
-	except:
+		code = request(baseUrl)
+	except Exception as e:
+		print(e)
 		print("ERROR:::The baseurl is not valid.")
 		sys.exit(1)
-
-	extUrls = getFuzzUrl()
-	# Start scanning
-	for extUrl in extUrls:
-		code = run(baseUrl+extUrl)
-		if code!=404:
-			print(str(code)+"-------"+baseUrl+extUrl)
+	getFuzzUrl()
+	for i in range(thread_num):
+		t = threading.Thread(target=scan)
+		t.start()
